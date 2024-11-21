028bd08d419617e4f491f91c310344e6550eb7eb
NurzhanNurdaulet <nurdabrat06@gmail.com>
2024-11-21 17:48:50 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("Bul Nurdaulettin computeri")
+#Kalkulator
+san=int(input())
+san2=int(input())
+amal=int(input())
+if amal == 1:
+    sum=san+san2
+    print(sum)
+elif amal == 2:
+    sum=san-san2
+    print(sum)
+elif amal == 3:
+    sum=san*san2
+    print(sum)
+elif amal == 4:
+    sum=san/san2
+    print(sum)
+else:
+    print("error")
+
+
+
+
